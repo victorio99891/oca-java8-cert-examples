04d418ba316818dfc78d38139cfb9c355548b343
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-11-06 10:05:36 +0100

    enum with all possibilities

--- enums/src/Main.java
+++ enums/src/Main.java
@@ -1,5 +1,22 @@
 package main;
 
+import main.ComboEnum;
+
 public class Main {
+
+    public static void main(String... args){
+
+        printEnum(ComboEnum.PICO);
+        printEnum(ComboEnum.MEDIA);
+        printEnum(ComboEnum.GRANDE);
+
+    }
+
+
+    public static void printEnum(ComboEnum sample){
+        System.out.println("\n"+sample.toString());
+        System.out.println("code="+sample.getSizeCode());
+        System.out.println("text="+sample.getText());
+    }
     
 }
